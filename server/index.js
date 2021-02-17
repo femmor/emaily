7d8686c2f7395abdbbca75d53903274b6a6450b5
femmor <fegomson@gmail.com>
2021-02-17 17:10:44 -0600

    Heroku deployment checklist

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
 
-const PORT = 5000;
-
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
