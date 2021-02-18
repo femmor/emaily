585b6f1810ae20830abcf81c81a5a6425c7ee7b4
femmor <fegomson@gmail.com>
2021-02-18 17:42:25 -0600

    Connecting to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require("./services/passport");
+
+// mongodb URI
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 // Get the authRoutes
