1b5b24614436846d3e06318e584b34487d949925
femmor <fegomson@gmail.com>
2021-03-03 17:53:19 -0600

    styled the stripe button

--- server/client/src/components/Payments.js
+++ server/client/src/components/Payments.js
@@ -7,14 +7,15 @@ class Payments extends Component {
       <>
         <StripeCheckout
           name="Emaily"
+          description="$5 for 5 email survey credits"
           image="https://www.flaticon.com/svg/vstatic/svg/4221/4221053.svg?token=exp=1614814929~hmac=0d3518c70cbd9cd71a3d3db4ebf8c840"
-          label="Add Credits"
-          email="info@emaily.co"
           panelLabel="Add 5 Credits for "
           amount={500}
           token={token => console.log(token)}
           stripeKey={process.env.REACT_APP_STRIPE_KEY}
-        />
+        >
+          <a class="waves-effect waves-light btn orange">Add Credits</a>
+        </StripeCheckout>
       </>
     );
   }
