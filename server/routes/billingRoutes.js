4fe4269ff727bd6e8b704d0f179463a6e89bc9ef
femmor <fegomson@gmail.com>
2021-03-08 17:24:55 -0600

    Created the charge object

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -3,6 +3,6 @@ const stripe = require("stripe")(keys.stripeSecretKey)
 
 module.exports = (app) => {
   app.post("/api/stripe", (req, res) => {
-    res.send(req.body.token)
+     console.log(req.body)
   })
 }
